b78e3509c2c55ec988c5dcb54f852eee9d7b93f3
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-14 08:56:27 +0700

    Initial commit

---
+++ SwiftUI T21/SwiftUI_T21App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T21App.swift
+//  SwiftUI T21
+//
+//  Created by M Fadli Zein on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
