ce684d44c968fba49e682085c8f43a4c57f207b7
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-14 09:16:38 +0700

    commit

--- SwiftUI T21/ContentView.swift
+++ SwiftUI T21/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tap = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Text("Delay 0.2 Second Animation! Bounce Effect")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
+        .frame(width: 200.0, height: 200.0)
+        .background(
+            LinearGradient(gradient:
+                            Gradient(colors: [Color.purple, Color.pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        )
+        .mask(RoundedRectangle(cornerRadius: 30))
+        .shadow(color: Color.purple, radius: 10, x: 0, y: 20)
+        .scaleEffect(tap ? 1.2 : 1)
+        .animation(.spring(response: 0.4, dampingFraction: 0.6))
+        .onTapGesture {
+            tap = true;
+            DispatchQueue.main.asyncAfter(deadline: .now()+5, execute: {
+                tap = false
+            })
+        }
+        
     }
 }
 
